b9c44d3b3df4e58b54ec0938a188764567dc7065
Sara Martucci <smartucci249@gmail.com>
2021-04-06 20:08:00 -0500

    SDK installed. Minsdk version increased from 19 to 21 to avoid dex file overload (below 20 will cause error or needs multidex library support. https://developer.android.com/studio/build/multidex Successful gradle build and emulator load.

---
+++ app/src/main/java/com/example/widgetshop_zettleintegration/Check_out.java
@@ -0,0 +1,14 @@
+package com.example.widgetshop_zettleintegration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Check_out extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_out);
+    }
+}
